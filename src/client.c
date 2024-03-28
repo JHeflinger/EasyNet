66f1f92ce7473768a9d254975f7f9a9c43fe7828
Jason Heflinger <jasonheflinger@gmail.com>
2024-03-28 03:27:03 -0400

    final cleanups

--- src/client.c
+++ src/client.c
@@ -72,33 +72,29 @@ void reset_prompt(EZN_BOOL logit) {
 		free(curr);
 	}
 }
+
 #ifdef __linux__
 int _kbhit(void) {
 	struct termios oldt, newt;
 	int ch;
 	int oldf;
- 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
-  ch = getchar();
- 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
- 
-  return 0;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+	if(ch != EOF) {
+		ungetc(ch, stdin);
+		return 1;
+	}
+ 	return 0;
 }
 #endif
+
 void input_handler(void* params) {
 	EZN_SOCKET serversock = ((Handler_args*)params)->server_socket;
 	size_t returnlen;
